826a61479f0ccfd040bf729b7ba5cd3e01f5f5d0
Cleeyv <cleeyv@riseup.net>
2020-11-09 01:34:01 -0500

    Tokio Notify sync to disable keyboard input events while file dialog is open

--- src/main.rs
+++ src/main.rs
@@ -18,9 +18,14 @@ use crossterm::{
 };
 use structopt::StructOpt;
 use tokio::stream::StreamExt;
+use tokio::sync::Notify;
 use tui::{backend::CrosstermBackend, Terminal};
 
+//writeln append debugging to file is commented out.
+//debug.txt file must be manually (re)created before use.
+//use std::fs::OpenOptions;
 use std::io::Write;
+use std::sync::Arc;
 
 #[derive(Debug, StructOpt)]
 struct Args {
@@ -43,12 +48,19 @@ async fn main() -> anyhow::Result<()> {
     let mut stdout = std::io::stdout();
     execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
 
+    let events = Arc::new(Notify::new());
+    let events2 = events.clone();
+
     let (tx, mut rx) = tokio::sync::mpsc::channel(100);
     tokio::spawn({
         let mut tx = tx.clone();
         async move {
             let mut reader = EventStream::new().fuse();
             while let Some(event) = reader.next().await {
+                //let mut debug = OpenOptions::new().append(true).open("debug.txt").unwrap();
+                //writeln!(debug, "(A)Await for event stream about to run...");
+                events2.notified().await;
+                //writeln!(debug, "(B)Await for event stream just ran...");
                 match event {
                     Ok(CEvent::Key(key)) => tx.send(Event::Input(key)).await.unwrap(),
                     Ok(CEvent::Resize(_, _)) => tx.send(Event::Resize).await.unwrap(),
@@ -59,7 +71,6 @@ async fn main() -> anyhow::Result<()> {
     });
 
     let backend = CrosstermBackend::new(stdout);
-
     let mut terminal = Terminal::new(backend)?;
 
     let signal_client = signal::SignalClient::from_config(app.config.clone());
@@ -68,6 +79,7 @@ async fn main() -> anyhow::Result<()> {
     terminal.clear()?;
 
     loop {
+        events.notify();
         terminal.draw(|f| ui::draw(f, &mut app))?;
         match rx.recv().await {
             Some(Event::Input(event)) => match event.code {
@@ -79,7 +91,24 @@ async fn main() -> anyhow::Result<()> {
                 KeyCode::Right => app.on_right(),
                 KeyCode::Down => app.on_down(),
                 KeyCode::Char('a') if event.modifiers.contains(KeyModifiers::CONTROL) => {
-                    app.on_attach();
+                    let events3 = events.clone();
+                    tokio::spawn(async move {
+                        //let mut debug = OpenOptions::new().append(true).open("debug.txt").unwrap();
+                        let open_file: String;
+                        //writeln!(debug, "File dialog about to run...");
+                        // TODO use the open_file_dialog_multi function
+                        match tinyfiledialogs::open_file_dialog("Attach file", "", None) {
+                            Some(file) => open_file = file,
+                            None => open_file = "null".to_string(),
+                        }
+                        println!("Open file {:?}", open_file);
+                        //writeln!(debug, "(2)Notify of events about to run...");
+                        events3.notify();
+                    });
+                    //let mut debug = OpenOptions::new().append(true).open("debug.txt").unwrap();
+                    //writeln!(debug, "(1)Await of events about to run...")?;
+                    events.notified().await;
+                    //writeln!(debug, "(3)Events just woke up from await!")?;
                     let size = terminal.size().unwrap();
                     terminal.resize(size)?;
                 }
