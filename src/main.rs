48fc27b1d158139d5830870aaad98051f0e3704d
Cleeyv <cleeyv@riseup.net>
2020-11-17 00:00:41 -0500

    Cleanup of async debugging, testing of dialog widgets

--- src/main.rs
+++ src/main.rs
@@ -21,9 +21,6 @@ use tokio::stream::StreamExt;
 use tokio::sync::Notify;
 use tui::{backend::CrosstermBackend, Terminal};
 
-//writeln append debugging to file is commented out.
-//debug.txt file must be manually (re)created before use.
-//use std::fs::OpenOptions;
 use std::io::Write;
 use std::sync::Arc;
 
@@ -57,10 +54,7 @@ async fn main() -> anyhow::Result<()> {
         async move {
             let mut reader = EventStream::new().fuse();
             while let Some(event) = reader.next().await {
-                //let mut debug = OpenOptions::new().append(true).open("debug.txt").unwrap();
-                //writeln!(debug, "(A)Await for event stream about to run...");
                 events2.notified().await;
-                //writeln!(debug, "(B)Await for event stream just ran...");
                 match event {
                     Ok(CEvent::Key(key)) => tx.send(Event::Input(key)).await.unwrap(),
                     Ok(CEvent::Resize(_, _)) => tx.send(Event::Resize).await.unwrap(),
@@ -91,24 +85,21 @@ async fn main() -> anyhow::Result<()> {
                 KeyCode::Right => app.on_right(),
                 KeyCode::Down => app.on_down(),
                 KeyCode::Char('a') if event.modifiers.contains(KeyModifiers::CONTROL) => {
-                    let events3 = events.clone();
-                    tokio::spawn(async move {
-                        //let mut debug = OpenOptions::new().append(true).open("debug.txt").unwrap();
-                        let open_file: String;
-                        //writeln!(debug, "File dialog about to run...");
-                        // TODO use the open_file_dialog_multi function
-                        match tinyfiledialogs::open_file_dialog("Attach file", "", None) {
-                            Some(file) => open_file = file,
-                            None => open_file = "null".to_string(),
-                        }
-                        println!("Open file {:?}", open_file);
-                        //writeln!(debug, "(2)Notify of events about to run...");
-                        events3.notify();
-                    });
-                    //let mut debug = OpenOptions::new().append(true).open("debug.txt").unwrap();
-                    //writeln!(debug, "(1)Await of events about to run...")?;
-                    events.notified().await;
-                    //writeln!(debug, "(3)Events just woke up from await!")?;
+                    //let open_file: String;
+                    // TODO use the open_file_dialog_multi function
+                    //match tinyfiledialogs::open_file_dialog("Attach file", "", None) {
+                    //    Some(file) => open_file = file,
+                    //    None => open_file = "null".to_string(),
+                    //}
+                    //println!("Open file {:?}", open_file);
+                    //
+                    tokio::process::Command::new("dialog")
+                        //.args(&["--inputbox", "name:", "10", "40"])
+                        .args(&["--fselect", "./", "0", "60"])
+                        .spawn()
+                        .expect("Command failed to start")
+                        .await?;
+                    events.notify();
                     let size = terminal.size().unwrap();
                     terminal.resize(size)?;
                 }
